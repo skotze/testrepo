c986cf6cb3cf24a9c949a3451021cb8da85ed4f6
Stephan Kotze <blah>
2016-08-20 19:34:43 +0100

    added env parameter of volume container so that jenkins can provide the volume name to sibling volumes it creates + added to .gitignore

--- devstackimage/compose/ci_stack/run.sh
+++ devstackimage/compose/ci_stack/run.sh
@@ -3,6 +3,7 @@ export JENKINS_CONTAINER_HOSTNAME=jenkins
 export REGISTRY_CONTAINER_HOSTNAME=registry
 export GITLAB_CONTAINER_HOSTNAME=gitlab
 export DOCKERHOST_HOSTNAME=$(hostname)
+export CI_STACK_VOLUME_NAME=ci_stack_volume
 
 mkdir -p $VOLUMES_DIR/jenkins/var/jenkins_home
 chown 1000 $VOLUMES_DIR/jenkins/var/jenkins_home
