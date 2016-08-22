c78730593030b66fa9b7eb3d7b1a067038339c75
Stephan Kotze <Stephanus.Kotze@gmail.com>
2016-08-22 14:05:29 +0100

    fixed gitlab registry vars update script

--- devstackimage/compose/ci_stack/gitlab_with_docker_registry/entry.sh
+++ devstackimage/compose/ci_stack/gitlab_with_docker_registry/entry.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sed  -i -e "s@#registry_external_url 'https://registry.gitlab.example.com'@registry_external_url 'https://registry.gitlab.example.com@" /etc/gitlab/gitlab.rb
+sed  -i -e "s@# registry_external_url.*@registry_external_url 'https://registry.gitlab.example.com@" /etc/gitlab/gitlab.rb
 sed  -i -e "s@registry_external_url.*@registry_external_url 'https://$DOCKERHOST_HOSTNAME:$REGISTRY_PORT'@" /etc/gitlab/gitlab.rb
+sed  -i -e "s@# registry_nginx\['ssl_certificate'\].*@registry_nginx['ssl_certificate'] = \"/certificates/server.crt\"@" /etc/gitlab/gitlab.rb
+sed  -i -e "s@# registry_nginx\['ssl_certificate_key'\].*@registry_nginx['ssl_certificate_key'] = \"/certificates/server.key\"@" /etc/gitlab/gitlab.rb
+
 exec /assets/wrapper
